77c3132e186eda6a08e773b927faf52e851b21f6
cskstudiosart <cs.k.studios.art@gmail.com>
2020-11-19 13:09:20 +0100

    small galery update

--- src/components/GaleryCards.js
+++ src/components/GaleryCards.js
@@ -6,37 +6,37 @@ function GaleryCards() {
     let categories = [
         {
             cid: '0', // id
-            ctext: 'Handmade on real paper.', // rövid leírás
+            ctext: 'Handmade on real paper', // rövid leírás
             clabel: 'Hand drawing', //név
             cpath: 'handdrawing' // hivatkozási cím
         },
         {
             cid: '1',
-            ctext: 'Made with a drawing tablet.',
+            ctext: 'Made with a drawing tablet',
             clabel: 'Digital art',
             cpath: 'digitalart'
         },
         {
             cid: '2',
-            ctext: 'Digital illustrations, logos.',
+            ctext: 'Digital illustrations, logos',
             clabel: 'Illustration',
             cpath: 'illustration'
         },
         {
             cid: '3',
-            ctext: '3D modelling, sculpting, texturing with Blender / Solid Edge, KeyShot.',
+            ctext: '3D modelling, sculpting, texturing with Blender / Solid Edge, KeyShot',
             clabel: 'Modelling',
             cpath: 'modelling'
         },
         {
             cid: '4',
-            ctext: '2D and 3D animations, short films.',
+            ctext: '2D and 3D animations, short films',
             clabel: 'Animation',
             cpath: 'animation'
         },
         {
             cid: '5',
-            ctext: 'Timelapse of my drawing, modelling.',
+            ctext: 'Timelapse of my drawing, modelling',
             clabel: 'Timelapse',
             cpath: 'timelapse'
         }
