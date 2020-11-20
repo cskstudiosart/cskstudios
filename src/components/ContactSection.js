e94be19a8e169f6ba3d32666bf3096a91f221372
cskstudiosart <cs.k.studios.art@gmail.com>
2020-11-20 12:15:40 +0100

    add contact banner to home page

---
+++ src/components/ContactSection.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import '../App.css';
+import './ContactSection.css';
+
+function ContactSection() {
+    return (
+        <>
+            <div className='contact_section' style={{backgroundImage: 'url(images/contact_section_banner_bg.png)'}}>
+                <div className='contact_section__container'>
+                    <div className='contact_section__wrapper'>
+                        <div className='contact_section__img' style={{backgroundImage: 'url(images/contact_section_banner.png)'}}></div>
+                        <div className='contact_section__data'>
+                            <div className='contact_section__mail'>
+                                <h2>Write a message by e-mail:</h2>
+                                <h2><b  className='contact_section__data_bold'>cs.k.studios.art@gmail.com</b></h2>
+                            </div>
+                            <div className='contact_section__phone'>
+                                <h2>Or call by phone:</h2>
+                                <h2><b  className='contact_section__data_bold'>+36/70-550-26-43</b></h2>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default ContactSection;
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,12 +3,14 @@ import '../../App.css';
 import HeroSection from '../HeroSection';
 import Cards from '../Cards';
 import Title from '../Title';
+import ContactSection from '../ContactSection';
 
 function Home() {
     Title('Home');
     return (
         <>
             <HeroSection />
+            <ContactSection />
             <Cards />
         </>
     );
